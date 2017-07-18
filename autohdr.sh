162e3e8b482e5dc0de4875de3c65182f720e371c
Andy Nitschke <none@none.com>
2017-07-18 19:35:10 -0400

    Setting stack size for HDR in the event that hugin can't figure it out

--- autohdr.sh
+++ autohdr.sh
@@ -16,10 +16,14 @@ echo "Processing directory $DIR"
 OUTPUT_FILE=$(ls $DIR/*.JPG | head -n 1 | sed -e 's/\.JPG$/_hdr.pto/')
 echo "creating '$OUTPUT_FILE'..."
 
-pto_gen $DIR/*.JPG -o $OUTPUT_FILE
+NUM_IMAGES=$(ls $DIR/*.JPG -l | wc -l)
+
+
+pto_gen $DIR/*.JPG -o -s $NUM_IMAGES $OUTPUT_FILE #-s to set stacks to avoid "enblend: excessive overlap detected" in the event that it can't automaticly detect stacks. But not -l so we don't link positions
 cpfind -o $OUTPUT_FILE $OUTPUT_FILE 
 cpclean -o $OUTPUT_FILE $OUTPUT_FILE 
 autooptimiser -p -o $OUTPUT_FILE $OUTPUT_FILE 
+
 
 # -p 0 => rectaliniar projection
 pano_modify -o $OUTPUT_FILE -p 0 --ldr-file=JPG --output-type=BF $OUTPUT_FILE
