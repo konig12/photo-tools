eac5867240fbe89e7e1bfc7016c31ebe5e7f195f
Andy Nitschke <none@none.com>
2017-08-01 23:15:21 -0400

    Adding option to change number of procs used by auto_pano_and_hdr.sh

--- auto_pano_and_hdr.sh
+++ auto_pano_and_hdr.sh
@@ -2,8 +2,24 @@
 
 #set -e
 
+while getopts hP:? opt
+do
+    case "$opt" in
+        P) NUM_PROCS=$OPTARG;;
+        h|\?) # unknown flag
+            echo >&2 "Usage: auto_pano_and_hdr.sh [-P] [-h] dir"
+            exit 1;;
+    esac
+done
+
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-find ./ -name 'hdr*' -print0 | xargs -0 -P 4 -I% $SCRIPTS_DIR/auto_hdr.sh %
-find ./ -name 'pano*' -print0 | xargs -0 -P 1 -I% $SCRIPTS_DIR/auto_pano.sh %
+if [ x$NUM_PROCS == x"" ]; then
+    find ./ -name 'hdr*' -print0 | xargs -0 -P 4 -I% $SCRIPTS_DIR/auto_hdr.sh %
+    find ./ -name 'pano*' -print0 | xargs -0 -P 1 -I% $SCRIPTS_DIR/auto_pano.sh %
+else
+    find ./ -name 'hdr*' -print0 | xargs -0 -P $NUM_PROCS -I% $SCRIPTS_DIR/auto_hdr.sh %
+    find ./ -name 'pano*' -print0 | xargs -0 -P $NUM_PROCS -I% $SCRIPTS_DIR/auto_pano.sh %
+fi
+
 
